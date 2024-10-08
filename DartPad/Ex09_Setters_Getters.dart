799d7ee5ebf2ecbad7b23c66aedea2288eb628ee
Amelipg <amelipg736@gmail.com>
2024-10-07 22:05:57 -0600

    Revert "Merge pull request #10 from Amelipg/Ameli-Practica12"
    This reverts commit a6f1f9690f2ba39e79e85808e5deb17b5f44a457, reversing
    changes made to 7f9663e4c668660bace295d54ba7360b8c0bf267.

--- DartPad/Ex09_Setters_Getters.dart
+++ DartPad/Ex09_Setters_Getters.dart
@@ -1,30 +1,34 @@
-void main(){
-  final mySquare = Square(side:10);
+void main() {
+  final mySquare = Square (side: 18);
+  
+ mySquare._side= -32;
+  
 
-  mySquare.side = 5;
-  print("El area es igual a: ${mySquare.area}");
+  print("""Lado del cuadrado: ${mySquare._side}
+  Área que opcupa: ${mySquare.calculateArea()}""");
+ 
 }
 
 class Square {
-  double _side;
   
-  Square({ required double side })
+  double _side;
+
+  Square({required double side})
     : _side = side;
-
-  double get area {
-      return _side * _side;
+  
+  double get area{
+    return _side * _side;
   }
-
-  set side(double value){
-    print("Asignando un nuevo valor al lado: $value");
-      if (value < 0) throw 'El valor del lado debe ser mayor a cero >= 0';
+  
+  set side( double value){
+    print('Asignando un nuevo valor al lado: $value');
+    if (value < 0) throw 'El valor del lado debe ser mayor a cero 0';
+    
     _side = value;
   }
-  
-  double calculateArea() {
-  return _side * _side;
+
+  double calculateArea()
+  {
+    return _side * _side;
   }
 }
-
-
-
